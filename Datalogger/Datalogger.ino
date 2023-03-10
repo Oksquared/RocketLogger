f20ee97d3dc8912f58e610d89a77671af69724f3
Owen Krumm <113589858+Oksquared@users.noreply.github.com>
2023-03-10 17:40:47 -0600

    added adafruit 388 barometer
    added adafruit 388 barometer, not sure it this code works

--- Datalogger/Datalogger.ino
+++ Datalogger/Datalogger.ino
@@ -4,23 +4,34 @@
   SD card datalogger for Century College rocket IMU datalogger
   This program logs the IMU data every (writeFrequency) miliseconds to the Adafruit SPI Flash SD Card - XTSD 512 MB memory card under the file name (fileName)
 
-code written by Owen Krumm 2023 for a Adafruit Featherwing M0 with a Adafruit LSM6DSOX + LIS3MDL FeatherWing - Precision 9-DoF IMU and  Adafruit SPI Flash SD Card - XTSD 512 MB chip.
+code written by Owen Krumm 2023 for a Adafruit Featherwing M0 with a Adafruit LSM6DSOX + LIS3MDL FeatherWing - Precision 9-DoF IMU and  Adafruit SPI Flash SD Card - XTSD 512 MB chip + Adafruit BMP388.
 
 Green light = good and running
 Red light = no IMU found
 Purple light = no SD memory found
 */
-
+#include "Adafruit_BMP3XX.h"
 #include <SPI.h>
 #include <SD.h>
 #include <Adafruit_LSM6DSOX.h>
+#include <Wire.h>
 const int chipSelectSD = A5;
 #define PIN 8        // On Trinket or Gemma, suggest changing this to 1
 #define NUMPIXELS 1  // Popular NeoPixel ring size
+
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11
+#define BMP_CS 10
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BMP3XX bmp;
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 Adafruit_LSM6DSOX sox;
 
-const int writeFrequency = 200; //log data every X ms
+
+const int writeFrequency = 200;  //log data every X ms
 
 String fileName = "data.txt";
 
@@ -38,12 +49,26 @@ void setup() {
   // see if the card is present and can be initialized:
   if (!SD.begin(chipSelectSD)) {
     Serial.println("Card failed, or not present");
-    pixels.setPixelColor(0, pixels.Color(25, 0, 25)); // purple means no memory card found.
+    pixels.setPixelColor(0, pixels.Color(25, 0, 25));  // purple means no memory card found.
     pixels.show();
     // don't do anything more:
     while (true) {
     }
-    
+  }
+  if (!bmp.begin_I2C()) {  // hardware I2C mode, can pass in address & alt Wire
+                           //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode
+                           //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
+    Serial.println("Could not find a valid BMP3 sensor, check wiring!");
+    pixels.setPixelColor(0, pixels.Color(25, 25, 0));  // yellow means no barometer.
+    while (1)
+      ;
+
+    //configure the BMP barometer
+    // Set up oversampling and filter initialization
+    bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+    bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+    bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+    bmp.setOutputDataRate(BMP3_ODR_50_HZ);
   }
 
   Serial.println("card initialized.");
@@ -70,6 +95,13 @@ void loop() {
     firstLine = false;
   }
 
+  if (!bmp.performReading()) {
+    Serial.println("Failed to perform reading :(");
+    return;
+  }
+
+
+
 
   // make a string for assembling the data to log:
   sensors_event_t accel;
@@ -77,23 +109,35 @@ void loop() {
   sensors_event_t temp;
   sox.getEvent(&accel, &gyro, &temp);
 
+  Serial.print(bmp.pressure / 100.0);
+  Serial.println(" hPa");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+
 
   // read three sensors and append to the string:
 
   dataString = "";
-  dataString += String(accel.acceleration.x);
+  dataString += String(accel.acceleration.x);  // Log X acceleration
   dataString += ",";
-  dataString += String(accel.acceleration.y);
+  dataString += String(accel.acceleration.y);  // Log Y acceleration
   dataString += ",";
-  dataString += String(accel.acceleration.z);
+  dataString += String(accel.acceleration.z);  // Log Z acceleration
   dataString += ",";
-  dataString += String(gyro.gyro.x);
+  dataString += String(gyro.gyro.x);  // Log X Rate
   dataString += ",";
-  dataString += String(gyro.gyro.x);
+  dataString += String(gyro.gyro.x);  // Log Y Rate
   dataString += ",";
-  dataString += String(gyro.gyro.x);
+  dataString += String(gyro.gyro.x);  //Log Z Rate
   dataString += ",";
-  dataString += String(temp.temperature);
+  dataString += String(temp.temperature);  // Log Temperature
+  dataString += ",";
+  dataString += String(bmp.pressure / 100.0);  // Log Raw pressure in hPa
+  dataString += ",";
+  dataString += String(bmp.readAltitude(SEALEVELPRESSURE_HPA));  // this is in meters
+
 
 
 
@@ -129,12 +173,12 @@ void configureLSM6DSOX() {
     //if (!sox.begin_SPI(LSM_CS)) {
     // if (!sox.begin_SPI(LSM_CS, LSM_SCK, LSM_MISO, LSM_MOSI)) {
     Serial.println("Failed to find LSM6DSOX chip");
-    pixels.setPixelColor(0, pixels.Color(25, 0, 0)); //red is bad
+    pixels.setPixelColor(0, pixels.Color(25, 0, 0));  //red is bad
     pixels.show();
     while (1) {
       delay(10);
     }
   }
-  pixels.setPixelColor(0, pixels.Color(0, 25, 0)); // green is good
+  pixels.setPixelColor(0, pixels.Color(0, 25, 0));  // green is good
   pixels.show();
 }
